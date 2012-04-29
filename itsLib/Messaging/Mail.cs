c3e70886d177a3681ab370c93ca55fd08c6664b4
Martin Sundhaug <martinsundhaug@gmail.com>
2012-04-29 18:59:59 +0200

    Created constructors for Messaging.Mail

--- itsLib/Messaging/Mail.cs
+++ itsLib/Messaging/Mail.cs
@@ -4,6 +4,15 @@ namespace itsLib.Messaging
 {
     public class Mail
     {
+        public Mail(string Path)
+        {
+        }
+
+        public Mail(uint Id, uint FolderId)
+            : this("/Messages/View_Message.aspx?MessageFolderID=" + FolderId.ToString() + "&MessageID=" + Id.ToString())
+        {
+        }
+
         private string From
         {
             get { throw new NotImplementedException(); }
