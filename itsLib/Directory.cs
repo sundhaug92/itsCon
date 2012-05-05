64df4420d89cc15d0dd68de5dc52c9e4eb43d10e
Martin Sundhaug <martinsundhaug@gmail.com>
2012-05-05 19:23:41 +0200

    Implemented Directory.Subdirectories, added FolderID (Id), fixed #5

--- itsLib/Directory.cs
+++ itsLib/Directory.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using HtmlAgilityPack;
 
 namespace itsLib
 {
@@ -10,11 +13,30 @@ namespace itsLib
     {
         Session Session;
         ICourseProjectCommons Parent;
+        uint Id;
 
-        public Directory(Session Session, ICourseProjectCommons Parent)
+        public Directory(Session Session, ICourseProjectCommons Parent, uint Id)
         {
             this.Session = Session;
             this.Parent = Parent;
+            this.Id = Id;
+        }
+
+        public Directory[] Subdirectories()
+        {
+            HtmlDocument Document = new HtmlDocument();
+            WebResponse resp = Session.GetHttpWebRequest("/Folder/process_folder.aspx?FolderID=" + Id.ToString()).GetResponse();
+            Document.Load(resp.GetResponseStream());
+            resp.Close();
+            var nodesWithHrefToDirectory = from node in Document.DocumentNode.DescendantNodes() where node.Name == "a" && node.GetAttributeValue("href", "").Contains("/Folder/process_folder.aspx") select node.GetAttributeValue("href", "");
+            Directory[] Subdirectories = new Directory[nodesWithHrefToDirectory.Count()];
+            uint i = 0;
+            foreach (string relPath in nodesWithHrefToDirectory)
+            {
+                Uri uri = relPath.StartsWith("/") ? new Uri(Properties.Settings.Default.urlBase + relPath) : new Uri(relPath);
+                Subdirectories[i++] = new Directory(Session, Parent, uint.Parse(HttpUtility.ParseQueryString(uri.Query).Get("FolderID")));
+            }
+            return Subdirectories;
         }
     }
 }
