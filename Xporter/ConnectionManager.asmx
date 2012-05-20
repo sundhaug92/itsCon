78b272b23e23ce55099bff06d56fbc9525889339
Martin Sundhaug <martinsundhaug@gmail.com>
2012-05-20 15:34:56 +0200

    Implemented Session.Create .setCustomer and .getCustomer for Xporter

---
+++ Xporter/ConnectionManager.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ConnectionManager.asmx.cs" Class="Xporter.ConnectionManager" %>
--- Xporter/Session.svc.cs
+++ Xporter/Session.svc.cs
@@ -5,18 +5,31 @@ namespace Xporter
 {
     [ServiceContract(Namespace = "")]
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-    public class ConnectionManager
+    public class Session
     {
+        itsLib.Session _Session;
+
         // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
         // To create an operation that returns XML,
         //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
         //     and include the following line in the operation body:
         //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
         [OperationContract]
-        public void DoWork()
+        public void Create()
         {
-            // Add your operation implementation here
-            return;
+            _Session = new itsLib.Session();
+        }
+
+        [OperationContract]
+        public void setCustomer(itsLib.Customer Customer)
+        {
+            _Session.Customer = Customer;
+        }
+
+        [OperationContract]
+        public itsLib.Customer getCustomer()
+        {
+            return _Session.Customer;
         }
 
         // Add more operations here and mark them with [OperationContract]
