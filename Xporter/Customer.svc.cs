dfe179400acebf9367918ea4ed872b14325b83b0
Martin Sundhaug <martinsundhaug@gmail.com>
2012-05-20 15:46:20 +0200

    Created Xporter/Customer.svc, for Customer information

---
+++ Xporter/Customer.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Xporter
+{
+    [ServiceContract(Namespace = "", SessionMode = SessionMode.Required)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Customer
+    {
+        itsLib.Customer _Customer;
+
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void fromName(string Name)
+        {
+            _Customer = new itsLib.Customer(new itsLib.Session(), Name);
+        }
+
+        [OperationContract]
+        public void fromId(uint Id)
+        {
+            _Customer = new itsLib.Customer(new itsLib.Session(), Id);
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
