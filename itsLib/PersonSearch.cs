36a75cb51b548c530e8ef69ea70ab901dc98412e
Martin Sundhaug <martinsundhaug@gmail.com>
2012-06-02 17:00:24 +0200

    Added PersonSearch constructor with PersonType

--- itsLib/PersonSearch.cs
+++ itsLib/PersonSearch.cs
@@ -8,7 +8,11 @@ namespace itsLib
 {
     internal class PersonSearch
     {
+        public PersonSearch(Session Session, string Forname, string Surname, int HierarchyId, int CourseId, PersonType PersonType)
+        {
+        }
         public PersonSearch(Session Session, string Forname, string Surname, int HierarchyId, int CourseId)
+            :this(Session, Forname, Surname, HierarchyId, CourseId, PersonType.administrator| PersonType.employee | PersonType.examinator | PersonType.guest| PersonType.parent| PersonType.student | PersonType.sysadmin)
         {
         }
 
