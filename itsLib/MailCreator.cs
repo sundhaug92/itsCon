4f35ef851e05bd64f437888209550dfe00e3b027
Martin Sundhaug <martinsundhaug@gmail.com>
2012-06-03 13:05:22 +0200

    Added MailCreator

---
+++ itsLib/MailCreator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace itsLib
+{
+    internal class MailCreator
+    {
+        public string Subject = "";
+
+        public Person Bcc
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Person Cc
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Person To
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void SendMessage()
+        {
+            /*
+             * /XmlHttp/Api.aspx?Function=MessagingValidateRecipients
+             * operationId: 1000
+             * to:
+             * cc:
+             * bcc:
+             * subject:
+             * text:
+             * files:
+             * id: 0
+             * messageMeasurement: 2
+             * _:
+            */
+        }
+    }
+}
