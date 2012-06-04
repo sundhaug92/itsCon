17080bf3cb1d29767281723f708082c1443fa835
Martin Sundhaug <martinsundhaug@gmail.com>
2012-06-04 22:20:46 +0200

    Added basic powershell support

---
+++ psitsl/NewCustomerCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace psitsl
+{
+    [Cmdlet(VerbsCommon.New, "ItslearningCustomer")]
+    internal class NewCustomerCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject();
+        }
+    }
+}
---
+++ psitsl/NewSessionCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace psitsl
+{
+    [Cmdlet(VerbsCommon.New, "ItslearningSession")]
+    public class NewSessionCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(new itsLib.Session());
+        }
+    }
+}
