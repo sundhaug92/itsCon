2354b5780e9645f1bdde108007e7a4b993dc09a7
Martin Sundhaug <martinsundhaug@gmail.com>
2012-06-18 21:23:47 +0200

    Implemented MailCreator.SendMessage

--- itsLib/MailCreator.cs
+++ itsLib/MailCreator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace itsLib
 {
@@ -29,6 +30,36 @@ namespace itsLib
              * messageMeasurement: 2
              * _:
             */
+            Dictionary<string, string> MailData = new Dictionary<string, string>(5);
+            string Persons = "";
+            foreach (Person Person in To)
+            {
+                Persons += Person.ShortName + ";";
+            }
+            MailData.Add("to", Persons);
+
+            Persons = "";
+            foreach (Person Person in Cc)
+            {
+                Persons += Person.ShortName + ";";
+            }
+            MailData.Add("cc", Persons);
+
+            Persons = "";
+            foreach (Person Person in Bcc)
+            {
+                Persons += Person.ShortName + ";";
+            }
+            MailData.Add("bcc", Persons);
+
+            MailData.Add("operationId", (1000).ToString());
+            MailData.Add("id", (0).ToString());
+            MailData.Add("text", Text);
+            MailData.Add("files", "");
+            MailData.Add("messageMeasurement", (2).ToString());
+            MailData.Add("_", "");
+            Session.PostData("/XmlHttp/Api.aspx?Function=MessagingValidateRecipients", MailData);
+            Session.PostData("/XmlHttp/Api.aspx?Function=MessagingSendMessage&MessageOperationID=" + (1000).ToString(), MailData);
         }
     }
 }
