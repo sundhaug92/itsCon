f55ed36de1b8954ee3429d921363e66477afbb5d
Martin Sundhaug <martinsundhaug@gmail.com>
2012-06-21 13:42:39 +0200

    Personalia is now loaded in background thread

--- itsLib/Person.cs
+++ itsLib/Person.cs
@@ -11,43 +11,41 @@ namespace itsLib
         private Customer _Customer;
         private uint _Id;
         private string _Name;
+        private System.Threading.Thread LoadingPersonaliaThread;
         private HtmlDocument Personalia;
         private Session sess;
 
         public Person(Session sess, uint Uid)
         {
-            Personalia = sess.GetDocument("/Person/show_person.aspx?PersonId=" + Uid.ToString() + "&Customer=" + sess.Customer.Id);
-            string Name = (from v in Personalia.DocumentNode.Descendants("span") where v.Id == "ctl00_PageHeader_TT" select v.InnerText).First();
-            if (Name.Contains('('))
-            {
-                Name = Name.Substring(0, Name.IndexOf('('));
-            }
-            Name = Name.Trim();
-            string Forename = Name.Substring(Name.IndexOf(", ") + ", ".Length);
-            string Surname = Name.Substring(0, Name.IndexOf(", "));
-            this._Customer = sess.Customer;
-            this._Name = Forename + " " + Surname;
             this._Id = Uid;
             this.sess = sess;
+            LoadingPersonaliaThread = new System.Threading.Thread(LoadPersonalia);
+            LoadingPersonaliaThread.Start();
         }
 
         public uint Id
         {
             get
             {
+                if (LoadingPersonaliaThread.IsAlive) LoadingPersonaliaThread.Join();
                 return _Id;
             }
         }
 
         public string Name
         {
-            get { return _Name; }
+            get
+            {
+                if (LoadingPersonaliaThread.IsAlive) LoadingPersonaliaThread.Join();
+                return _Name;
+            }
         }
 
         public Uri PublicFilesBaseUri
         {
             get
             {
+                if (LoadingPersonaliaThread.IsAlive) LoadingPersonaliaThread.Join();
                 return new Uri("http://files.itslearning.com/data/" + this._Customer.Id.ToString() + "/" + _Id.ToString());
             }
         }
@@ -56,6 +54,7 @@ namespace itsLib
         {
             get
             {
+                if (LoadingPersonaliaThread.IsAlive) LoadingPersonaliaThread.Join();
                 var _shortname = "";
                 var ShortNames = (from node in Personalia.DocumentNode.DescendantNodes() where node.GetAttributeValue("onclick", "").Contains("messages/sendmessage.aspx") select node.GetAttributeValue("onclick", "").Substring(node.GetAttributeValue("onclick", "").IndexOf("TextTo=") + "TextTo=".Length));
                 if (ShortNames.Count() > 0) _shortname = ShortNames.First().Substring(0, ShortNames.First().Length - 2);
@@ -76,6 +75,24 @@ namespace itsLib
             Uri user_info_Uri = new Uri(Properties.Settings.Default.urlBase + e.First());
             Uid = uint.Parse(HttpUtility.ParseQueryString(user_info_Uri.Query).Get("PersonId"));
             return new Person(sess, Uid);
+        }
+
+        public void LoadPersonalia()
+        {
+            lock (this)
+            {
+                Personalia = sess.GetDocument("/Person/show_person.aspx?PersonId=" + _Id.ToString() + "&Customer=" + sess.Customer.Id);
+                string Name = (from v in Personalia.DocumentNode.Descendants("span") where v.Id == "ctl00_PageHeader_TT" select v.InnerText).First();
+                if (Name.Contains('('))
+                {
+                    Name = Name.Substring(0, Name.IndexOf('('));
+                }
+                Name = Name.Trim();
+                string Forename = Name.Substring(Name.IndexOf(", ") + ", ".Length);
+                string Surname = Name.Substring(0, Name.IndexOf(", "));
+                this._Customer = sess.Customer;
+                this._Name = Forename + " " + Surname;
+            }
         }
     }
 }
